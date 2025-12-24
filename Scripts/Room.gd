cce80ba159803ed31f9c686ceacffd2038839931
mrjahmez <mrjahmez@gmail.com>
2025-12-25 00:23:20 +1100

    Added camera animations and UI for interacting with characters

--- Scripts/Room.gd
+++ Scripts/Room.gd
@@ -8,15 +8,21 @@ var room_id: int
 var floor_num: int
 var characters: Array[Character]
 var interactables: Array[Interactable]
-var texture: Texture2D
 var model: MeshInstance3D
 
-func _init(name_to: String, id: int, floor_to: int = 0, rooms: Array[Room] = []):
+#var doors: Array[MeshInstance3D]
+#var handler
+
+func _init(name_to: String, id: int, floor_to: int = 0, rooms: Array[Room] = [], game_handler = null):
 	room_name = name_to
 	room_id = id
 	floor_num = floor_to
 	linked_rooms += rooms
-	texture = TextureSampler.new().getTexture()
+	#handler = get_node_or_null("/root/GameHandler") # fix this to instantiate rooms
+	#print(handler)
+
+func _ready() -> void:
+	pass
 	
 func addRoom(room: Room):
 	linked_rooms.append(room)
@@ -32,6 +38,23 @@ func removeCharacter(character_name: String):
 		if character.getName() == character_name:
 			characters.erase(character)
 			break
+
+#func updateDoors(is_room: bool = false) -> void:
+#	if doors.size() == 0 or doors == null:
+#		for i in range(linked_rooms.size()):
+#			var new_door = MeshInstance3D.new()
+#			#handler.add_child(new_door)
+#			new_door.mesh = load("res://Models/Door.tres")
+#			
+#			new_door.global_position = Vector3(-19.0, 2.25, (18 / linked_rooms.size()) * -i)
+#			doors.append(new_door)
+#			
+#	if is_room:
+#		for door in doors:
+#			door.visible = true
+#	else:
+#		for door in doors:
+#			door.visible = false
 	
 func getName() -> String:
 	return room_name
@@ -44,9 +67,6 @@ func getFloor() -> int:
 	
 func getLinkedRooms() -> Array[Room]:
 	return linked_rooms
-	
-func getTexture() -> Texture2D:
-	return texture
 	
 func getCharacters() -> Array[Character]:
 	return characters
